80b069c4eee9c73c6896a2de9a02c349ca734fa7
Martin <m2tk16@gmail.com>
2024-12-14 16:30:52 -0600

    Added a few images to the portfolio which are now being brought in by the previously migrated S3 Image import. Updates some previously created html tags to bootstraps elements for continunity.

--- src/Portfolio/portfolio.jsx
+++ src/Portfolio/portfolio.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 import "./portfolio.css";
 import Container from 'react-bootstrap/Container';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import PortfolioWrapper from './portfolioWrapper';
+
 
 const Portfolio = () => {
     const [imageArray] = useState([
-        "images/home/into_the_fog.jpg",
-        "images/home/into_the_fog.jpg",
-        "images/home/into_the_fog.jpg"
+        "images/portfolio/wonderland.webp",
+        "images/portfolio/magenta_sky.webp",
+        "images/portfolio/stamina_shroom.webp",
+        "images/portfolio/frozen.webp",
+        "images/portfolio/floating_rocks.webp",
+        "images/portfolio/river_run.webp",
+        "images/portfolio/loki.webp"
     ]);
 
+    
     return (
         <Container>
-            <div className="row g-2 justify-content-evenly">
+            <Row>
                 {imageArray.map((image, index) => (
-                    <div key={index} className="col-10 justify-content-evenly col-10-spacer">
-                        <PortfolioWrapper path={image} />
-                    </div>
+                    <Row className="mb-4">
+                        <Col className="col-12" key={index}>
+                            <PortfolioWrapper path={image} />
+                        </Col>
+                    </Row>
                 ))}
-            </div>
+            </Row>
         </Container>
     );
 };
--- src/Portfolio/portfolioWrapper.jsx
+++ src/Portfolio/portfolioWrapper.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./portfolio.css";
 import GetImage from "../Utilities/getImage";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 interface portfolioProps {
     path: string;
@@ -10,8 +12,19 @@ const PortfolioWrapper = (props: portfolioProps) => {
     const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
-        setIsVisible(true); // Set visibility only after the component mounts
+        setIsVisible(true);
     }, []);
+
+    const extractAndFormat = (path) => {
+        const extracted = path.split("/")[2].split(".")[0];
+        const formatted = extracted.replace(/_/g, " ");
+        const capitalized = formatted
+          .split(" ")
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ");
+      
+        return capitalized;
+      };
 
     return (
         <div className="p-6 portfolio-image-wrapper">
@@ -19,13 +32,13 @@ const PortfolioWrapper = (props: portfolioProps) => {
                 imagePath={props.path}
                 className={`home-image ${isVisible ? "visible" : ""}`}
             />
-            <div className="column">
-                <div className="row">
-                    <div className="col-12 text-center">Title: Title</div>
-                </div>
+            <Col>
+                <Row className="mt-3">
+                    <div className="col-12 text-center">{extractAndFormat(props.path)}</div>
+                </Row>
                 <hr />
-                <div className="row portfolio-image-text">
-                    <div className="col-4 text-center">
+                <Row className="mt-4">
+                    <Col className="col-4 text-center">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="40"
@@ -36,8 +49,8 @@ const PortfolioWrapper = (props: portfolioProps) => {
                         >
                             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
                         </svg>
-                    </div>
-                    <div className="col-4 text-center">
+                    </Col>
+                    <Col className="col-4 text-center">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="40"
@@ -48,8 +61,8 @@ const PortfolioWrapper = (props: portfolioProps) => {
                         >
                             <path d="M5.5 13v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.5v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.084c1.992 0 3.416-1.033 3.416-2.82 0-1.502-1.007-2.323-2.186-2.44v-.088c.97-.242 1.683-.974 1.683-2.19C11.997 3.93 10.847 3 9.092 3H9V1.75a.25.25 0 0 0-.25-.25h-1a.25.25 0 0 0-.25.25V3h-.573V1.75a.25.25 0 0 0-.25-.25H5.75a.25.25 0 0 0-.25.25V3l-1.998.011a.25.25 0 0 0-.25.25v.989c0 .137.11.25.248.25l.755-.005a.75.75 0 0 1 .745.75v5.505a.75.75 0 0 1-.75.75l-.748.011a.25.25 0 0 0-.25.25v1c0 .138.112.25.25.25zm1.427-8.513h1.719c.906 0 1.438.498 1.438 1.312 0 .871-.575 1.362-1.877 1.362h-1.28zm0 4.051h1.84c1.137 0 1.756.58 1.756 1.524 0 .953-.626 1.45-2.158 1.45H6.927z" />
                         </svg>
-                    </div>
-                    <div className="col-4 text-center">
+                    </Col>
+                    <Col className="col-4 text-center">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -60,9 +73,9 @@ const PortfolioWrapper = (props: portfolioProps) => {
                         >
                             <path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.35.35 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91q.57-.403.993-1.005a4.94 4.94 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.7 2.7 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.7.7 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016q.326.186.548.438c.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.87.87 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.35.35 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32z" />
                         </svg>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </Col>
         </div>
     );
 };
