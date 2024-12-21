cab95505c9db08e2f20861540b0700290e2969ec
Martin <m2tk16@gmail.com>
2024-12-20 22:53:37 -0600

    Removed unused files and updated sondra names in the about.

---
+++ src/Subscribe/subscribe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./subscribe.css";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+const Subscribe = () => {
+    return (
+        <>
+            <Row>
+                <Col md={12}>
+                    <div className="content-wrapper">
+                        Subscribe here
+                    </div>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default Subscribe;
---
+++ src/Unsubscribe/unsubscribe.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./unsubscribe.css";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+const Unubscribe = () => {
+    return (
+        <>
+            <Row>
+                <Col md={12}>
+                    <div className="content-wrapper">
+                        Unubscribe here
+                    </div>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default Unubscribe;
